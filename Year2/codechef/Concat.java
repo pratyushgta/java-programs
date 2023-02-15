7f9408edd153e071c2cc4b40c83ac4e8129ab284
Pratyush Kumar <pratyushgta@gmail.com>
2023-02-15 22:29:50 +0530

    Update #14 15-02-2022

--- Year2/codechef/Concat.java
+++ Year2/codechef/Concat.java
@@ -1,39 +1,40 @@
-//NOT COMPLETE
 package Year2.codechef;
 
 import java.util.*;
 
 class Concat {
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        int T = sc.nextInt(); // number of test cases
-        while (T-- > 0) {
-            int n = sc.nextInt(); // number of strings
-            String[] arr = new String[n];
-            for (int i = 0; i < n; i++) {
-                arr[i] = sc.next();
+    public static boolean canConcatPalindrome(String a, String b) {
+        String s = a + b;
+        int oddChars = 0;
+        Map<Character, Integer> freq = new HashMap<>();
+        for (char c : s.toCharArray()) {
+            freq.put(c, freq.getOrDefault(c, 0) + 1);
+            if (freq.get(c) % 2 == 1) {
+                oddChars++;
+            } else {
+                oddChars--;
             }
-            int numEven = 0, numOdd = 0;
-            for (int i = 0; i < n; i++) {
-                if (isPalindrome(arr[i])) {
-                    numEven++;
-                } else {
-                    numOdd++;
-                }
-            }
-            int ans = numEven * (numEven - 1) / 2 + numEven * numOdd;
-            System.out.println(ans);
         }
+        return oddChars <= 1;
     }
 
-    // utility method to check if a string is a palindrome
-    private static boolean isPalindrome(String s) {
-        int n = s.length();
-        for (int i = 0; i < n / 2; i++) {
-            if (s.charAt(i) != s.charAt(n - i - 1)) {
-                return false;
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int T = sc.nextInt();
+        sc.nextLine();
+        while (T > 0) {
+            int n = sc.nextInt();
+            int m = sc.nextInt();
+            sc.nextLine();
+            String a = sc.nextLine();
+            String b = sc.nextLine();
+
+            if (canConcatPalindrome(a, b)) {
+                System.out.println("YES");
+            } else {
+                System.out.println("NO");
             }
+            T--;
         }
-        return true;
     }
 }
