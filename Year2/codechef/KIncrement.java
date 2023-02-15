8d45c80c9cda1265e048f2a4df73a53d6af86e2c
Pratyush Kumar <pratyushgta@gmail.com>
2023-02-15 22:49:32 +0530

    Update #14 15-02-2022

---
+++ Year2/codechef/KIncrement.java
@@ -0,0 +1,65 @@
+package Year2.codechef;
+
+import java.util.Scanner;
+
+public class KIncrement {
+    public static boolean canMakePalindrome(int[] A, int K) {
+        int oddCount = 0;
+        for (int x : A) {
+            if (x % 2 == 1) {
+                oddCount++;
+            }
+        }
+        if (oddCount > 1) {
+            return false;
+        }
+        if (K > A.length) {
+            return false;
+        }
+        if (K == 1) {
+            return true;
+        }
+        if (K % 2 == 0) {
+            boolean allEven = true;
+            for (int x : A) {
+                if (x % 2 == 1) {
+                    allEven = false;
+                    break;
+                }
+            }
+            if (allEven) {
+                return true;
+            }
+        } else {
+            boolean hasEven = false;
+            for (int x : A) {
+                if (x % 2 == 0) {
+                    hasEven = true;
+                    break;
+                }
+            }
+            if (!hasEven) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int T = scanner.nextInt();
+        for (int t = 0; t < T; t++) {
+            int N = scanner.nextInt();
+            int K = scanner.nextInt();
+            int[] A = new int[N];
+            for (int i = 0; i < N; i++) {
+                A[i] = scanner.nextInt();
+            }
+            if (canMakePalindrome(A, K)) {
+                System.out.println("YES");
+            } else {
+                System.out.println("NO");
+            }
+        }
+    }
+}
