fd0f3add4bb689eb168dffcccd5030120405199a
Pratyush Kumar <pratyushgta@gmail.com>
2023-02-15 21:34:31 +0530

    Update #14 15-02-2022

---
+++ Year2/codechef/Tekken.java
@@ -0,0 +1,27 @@
+package Year2.codechef;
+
+import java.util.Scanner;
+
+public class Tekken {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int T = sc.nextInt();
+        sc.nextLine();
+        while (T > 0) {
+            String[] input = sc.nextLine().split("\\s+");
+            int A = Integer.parseInt(input[0]);
+            int B = Integer.parseInt(input[1]);
+            int C = Integer.parseInt(input[2]);
+
+            int min = Math.min(B, C);
+            B -= min;
+            C -= min;
+
+            if (A > B && A > C) {
+                System.out.println("YES");
+            } else
+                System.out.println("NO");
+            T--;
+        }
+    }
+}
