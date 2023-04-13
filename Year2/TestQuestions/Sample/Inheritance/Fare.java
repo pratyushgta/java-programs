c8582a0ed943ce2541700783a86e248500bd0d1e
Pratyush Kumar <pratyushgta@gmail.com>
2023-04-13 21:22:50 +0530

    Update #28 13-04-2022

--- Year2/TestQuestions/Sample/Inheritance/Fare.java
+++ Year2/TestQuestions/Sample/Inheritance/Fare.java
@@ -1,16 +1,44 @@
 package Year2.TestQuestions.Sample.Inheritance;
 
-class Vehicle{
+import java.util.Scanner;
 
+class Vehicle{
+    int seating_capacity;
+
+    Vehicle(int cap){
+        seating_capacity = cap;
+    }
+    double getFare(){
+        return seating_capacity*100;
+    }
 }
 
 class Bus extends Vehicle{
-    int seating_capacity;
+    Bus(int cap){
+        super(cap);
+    }
+    double getFare(){
+        return (super.getFare()+(super.getFare()*0.1));
+    }
 }
 
 class Bike extends Vehicle{
-
+    Bike(int cap){
+        super(cap);
+    }
+    double getFare(){
+        return super.getFare();
+    }
 }
 public class Fare {
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter seating capacity: ");
+        int s_cap = sc.nextInt();
 
+        Bus ob = new Bus(s_cap);
+        System.out.println("Fare of bus: "+ob.getFare());
+        Bike ob1 = new Bike(s_cap);
+        System.out.println("Fare of bike: "+ob1.getFare());
+    }
 }
