bce9669b5c5070e61ce3ac1c82bbeed49879e54d
Pratyush Kumar <pratyushgta@gmail.com>
2023-10-30 23:45:12 +0530

    Update #38 30-10-2023

---
+++ Year3/DiskSchedulingAlgorithm.java
@@ -0,0 +1,99 @@
+/**
+ * This class contains methods for implementing FCFS & SSTF- Disk Scheduling algorithms
+ * OS-E10
+ *
+ * @author Pratyush Kumar (github.com/pratyushgta)
+ */
+package Year3;
+
+import java.util.Scanner;
+
+public class DiskSchedulingAlgorithm {
+    int size, head;
+    int[] diskReq;
+
+    void input() {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter total no. of disk requests: ");
+        size = scanner.nextInt();
+
+        diskReq = new int[size];
+        System.out.println("\n>>>>Input Disk Request Queue<<<<");
+        for (int i = 0; i < size; i++) {
+            diskReq[i] = scanner.nextInt();
+        }
+
+        System.out.print("Enter initial head position: ");
+        head = scanner.nextInt();
+    }
+
+    void FCFS() {
+        int totalSeekTime = 0;
+        int currentHead = head;
+
+        for (int i = 0; i < size; i++) {
+            int request = diskReq[i];
+            totalSeekTime += Math.abs(request - currentHead);
+            currentHead = request;
+        }
+
+        System.out.println("Total seek time using FCFS: " + totalSeekTime);
+    }
+
+    void SSTF() {
+        int totalSeekTime = 0;
+        int currentHead = head;
+
+        boolean[] visited = new boolean[size];
+        int requestsLeft = size;
+
+        while (requestsLeft > 0) {
+            int minSeek = Integer.MAX_VALUE;
+            int nextIndex = -1;
+
+            for (int i = 0; i < size; i++) {
+                if (!visited[i]) {
+                    int seek = Math.abs(diskReq[i] - currentHead);
+                    if (seek < minSeek) {
+                        minSeek = seek;
+                        nextIndex = i;
+                    }
+                }
+            }
+
+            visited[nextIndex] = true;
+            totalSeekTime += minSeek;
+            currentHead = diskReq[nextIndex];
+            requestsLeft--;
+        }
+
+        System.out.println("Total seek time using SSTF: " + totalSeekTime);
+    }
+
+    void menu() {
+        Scanner sc = new Scanner(System.in);
+        while (true) {
+            System.out.print("\n>>>> DISK SCHEDULING ALGORITHMS <<<<\n1. FCFS\n2. SSTF\n3. Exit\n\nEnter choice: ");
+            int choice = sc.nextInt();
+
+            switch (choice) {
+                case 1:
+                    FCFS();
+                    break;
+                case 2:
+                    SSTF();
+                    break;
+                case 3:
+                    System.exit(0);
+                default:
+                    System.out.println("\nHeh! You thought this hasn't been accounted for?");
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        DiskSchedulingAlgorithm ob = new DiskSchedulingAlgorithm();
+        ob.input();
+        ob.menu();
+    }
+}
---
+++ Year3/gfg.java
@@ -0,0 +1,45 @@
+package Year3;
+
+class GFG {
+    static int size = 8;
+
+    static void FCFS(int arr[], int head) {
+        int seek_count = 0;
+        int distance, cur_track;
+
+        for (int i = 0; i < size; i++) {
+            cur_track = arr[i];
+
+            // calculate absolute distance
+            distance = Math.abs(cur_track - head);
+
+            // increase the total count
+            seek_count += distance;
+
+            // accessed track is now new head
+            head = cur_track;
+        }
+
+        System.out.println("Total number of " +
+                "seek operations = " +
+                seek_count);
+
+        // Seek sequence would be the same
+        // as request array sequence
+        System.out.println("Seek Sequence is");
+
+        for (int i = 0; i < size; i++) {
+            System.out.println(arr[i]);
+        }
+    }
+
+    // Driver code
+    public static void main(String[] args) {
+        // request array
+        int arr[] = {98, 183, 37, 122, 14, 12};
+        int head = 50;
+
+        FCFS(arr, head);
+    }
+}
+
