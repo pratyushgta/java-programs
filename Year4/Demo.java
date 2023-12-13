5baeee2ec3cd5335bf4a27864eeee3a3ba81cb61
Pratyush Kumar <pratyushgta@gmail.com>
2023-12-13 14:57:22 +0530

    Update #39 13-12-2023

--- Year4/Demo.java
+++ Year4/Demo.java
@@ -2,8 +2,12 @@ package Year4;
 
 public class Demo {
 
-    void meth2() {
+    void meth1() {
         System.out.println("This is under Demo class");
+    }
+
+    String meth2(String str) {
+        return str;
     }
 
     void meth3(int[] array) {
@@ -36,32 +40,30 @@ public class Demo {
     public static void main(String[] args) {
         Demo ob = new Demo();
 
-        // EX.0
-        System.out.println("Output of Ex0:");
-        ob.meth2();
+        System.out.println("Output of meth1:");
+        ob.meth1();
 
-        // EX.1
-        System.out.println("\nOutput of Ex1:");
+        System.out.println("\nOutput of meth2:");
+        String res = ob.meth2("Apples");
+        System.out.println(res);
+
+        System.out.println("\nOutput of meth3:");
         int[] arr = {1, 2, 3, 4};
         ob.meth3(arr);
 
-        // EX.2
-        System.out.println("\nOutput of Ex2:");
+        System.out.println("\nOutput of meth4:");
         String[] arr1 = ob.meth4();
         System.out.println(arr1[0]);
 
-        // EX.3
-        System.out.println("\nOutput of Ex3:");
+        System.out.println("\nOutput of meth5:");
         ABC obj = ob.meth5();
         obj.m();
 
-        // EX.4
-        System.out.println("\nOutput of Ex4:");
+        System.out.println("\nOutput of meth6:");
         ABC obj1 = new ABC();
         ob.meth6(obj1);
 
-        // EX.5
-        System.out.println("\nOutput of Ex5:");
+        System.out.println("\nOutput of meth7:");
         ABC[] arr2 = ob.meth7();
         System.out.println(arr2[1]);
         arr2[1].m();
